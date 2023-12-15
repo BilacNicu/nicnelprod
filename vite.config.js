e59da00fb823a244fa1480bd569aa780ad07fcc7
Nicu <nicubilac@yahoo.com>
2023-12-15 13:26:14 +0200

    8th commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/nicnelprod/",
   plugins: [react()],
+  optimizeDeps: {
+    include: ['*.png', '*.jpg', '*.mp4'] 
+  }
 })
